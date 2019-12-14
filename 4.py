6ea49a22fc1bc89c17d81226e634aa561a733f59
Cindy Mendoza <cindy.mendoza@harrison.com.pe>
2019-12-14 14:17:36 -0500

    ejercicio cuatro fix

--- 4.py
+++ 4.py
@@ -1,9 +1,13 @@
 lista=[]
 bandera = True
-valor = int(input('Ingresa un numero:'))
-lista.append(valor)
+while bandera:
+  valor = int(input('Ingresa un numero:'))
+  lista.append(valor)
+  for x in lista:
+   print(x) 
+   
+  if valor == 0:
+   bandera = False
+   break
 
 
-
-for x in range(1,11):
-  print(valor*x)
