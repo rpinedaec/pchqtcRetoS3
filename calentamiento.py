6fc5f4b39db7c1cb0dd9f67a558349a7cab54812
mauricioa2601 <mauricioa2601@gmail.com>
2019-12-14 10:44:42 -0500

    solucion del primer ejercicio

---
+++ calentamiento.py
@@ -0,0 +1,10 @@
+##Mete los valores del 1 al 100 en una lista.
+num=[]
+for i in range(100):
+    num.append(i+1)
+
+print(num)
+
+##Crea una lista con los meses del año, pide números al usuario, si el numero esta entre 1 
+# y la longitud máxima de la lista, muestra el contenido de esa posición sino muestra un mensaje de error.
+#  El programa termina cuando el usuario introduce un cero
