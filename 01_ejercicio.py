4c203249e4bf5ed2779f094516452105fd913197
Sebastian Yabiku <sebastian.yabiku@pachaqtec.edu.pe>
2019-12-14 11:34:44 -0500

    Solucion del ejercicio numero 1

---
+++ 01_ejercicio.py
@@ -0,0 +1,15 @@
+# Declaramos la lista en blanco
+numeros = []
+
+# Declaramos el limite en 1
+limit = 1
+
+# Mientras el limite no sea mayor que 100, se ejecutara el while
+while limit <= 100:
+    # Agregando el numero a la lista declarada inicialmente
+    numeros.append(limit)
+    # Aumentando en 1 la variable limit
+    limit = limit + 1
+
+# Imprimiendo los resultados
+print(numeros)
