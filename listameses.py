ebecc04ee39a862af99ae2d969d69d5d197ce10d
Eduardo Hidalgo <eduardo.hidalgo.crespo@gmail.com>
2019-12-14 12:15:04 -0500

    ejericicios 1 y 2

--- listameses.py
+++ listameses.py
@@ -1,19 +1,17 @@
 
-meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Setiembre','Ocutubre','Noviembre','Diciembre']
-print (meses)
-flag = 1
+meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+         'Julio', 'Agosto', 'Setiembre', 'Ocutubre', 'Noviembre', 'Diciembre']
+print(meses)
+flag = True
 
-while flag == 1:
-    mes = int (input('ingresa un numero que represente mes del anio'))
-    if (mes > 12):
-        print ('Dato errado, ingrese datos del 1 - 12')
+while flag == True:
+    mes = int(input('ingresa un numero que represente mes del anio'))
+
+    if mes > 12:
+        print('Error')
     else:
-        if (mes == 0): 
-            flag = 99
-print(meses(mes-1))
-        
-    
-    
- 
-
-
+        if mes == 0:
+            flag = False
+        else:
+            index = mes-1
+    print(meses[index])
