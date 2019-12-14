db5e13075e10cb97ee1b88f199cd5e17c3ec6b73
Javier Cardenas <javiercs893@gmail.com>
2019-12-14 11:13:54 -0500

    ejercicio01 listo

---
+++ ejercicio01.py
@@ -0,0 +1,9 @@
+# 1) Mete los valores del 1 al 100 en una lista.
+lista = []
+
+x = 1
+for x in range (1,100):
+    x=x+1
+    print(x)
+
+
---
+++ ejercicio02.py
@@ -0,0 +1,11 @@
+meses = ["e","f","m","a"]
+
+bandera=1
+while bandera == 1:
+    mes = input('Ingresa un mes')
+    if mes > 12
+    print('Error')
+    else
+    print(meses[mes-1])
+    if mes = 0
+    print(meses[0+1])
