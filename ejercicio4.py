8b970f46163b487e826fd087fa80393cffbf9cc0
Eduardo Hidalgo <eduardo.hidalgo.crespo@gmail.com>
2019-12-14 15:00:09 -0500

    cuarto ejercicio

---
+++ ejercicio4.py
@@ -0,0 +1,11 @@
+lista = []
+ 
+flag = True
+while flag == True:
+    numero = int(input("Ingrese un numero: "))
+    if(numero == 0):
+        flag = False  
+    else:
+        lista.append(numero)
+ 
+print(lista)
