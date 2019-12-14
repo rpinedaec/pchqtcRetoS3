5a25c471e832a323d0dddabb8742db27f6148792
Rodrigo Bruce Galvez <rodrigobrucegalvez@gmail.com>
2019-12-14 11:10:23 -0500

    Cuatro primeros ejercicios

---
+++ ejercicios1.py
@@ -0,0 +1,45 @@
+# paises = []
+
+# while (len(paises)<6):
+#     pais = input("Ingrese un pais: ")
+#     paises.append(pais)
+
+# valor = input("Ingrese el valor a contar: ")
+# print(paises)
+# print(paises.count(valor))
+
+# Uno al cien:
+unoalcien = []
+
+for i in range(100):
+    unoalcien.append(i+1)
+
+print(unoalcien)
+
+# Meses
+
+meses = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre']
+mes = int(input("Ingrese un numero del 1 al 12 para ver que mes es: "))
+print(meses[mes-1])
+
+
+# Tabla de multiplicar
+
+base = int(input("Ingresa un numero para devolver su tabla de multiplicar: "))
+tablademult = []
+
+
+for i in range(10):
+    tablademult.append(base*(i+1))
+
+print(tablademult)
+
+# Pedir numeros
+
+listaNumeros = []
+
+while True:
+    numero = int(input("Ingresa un numero: "))
+
+
+
