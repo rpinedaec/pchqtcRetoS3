24862d802d8470b3a83a362cd71594185036ebc0
Eduardo Hidalgo <eduardo.hidalgo.crespo@gmail.com>
2019-12-14 11:15:10 -0500

    2 ejercicios

---
+++ listameses.py
@@ -0,0 +1,21 @@
+
+meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Setiembre','Ocutubre','Noviembre','Diciembre']
+print (meses)
+mes = []
+flag = 1
+
+while flag == 1:
+    meses = input('ingresa un numero que represente mes del anio')
+    if (meses>12):
+        print ('Dato errado')
+        
+    else:
+        if (mes ==0): 
+        flag = 99
+    
+        print(meses(mes-1))
+    
+    
+ 
+
+
