4adeeb6b17a1869a8762951167ca867b45091110
ericksajami <ericksajami@gmail.com>
2019-12-18 21:05:55 -0500

    Resolucion del Ejercicio 2 del Reto de la semana 3

---
+++ Ejercicio_02.py
@@ -0,0 +1,24 @@
+# Creacion de la lista con los meses del año
+meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
+#Creacion de una vaariable bandera para ejecutar el while.
+bandera = True
+
+# Mientras la bandera sea True, se ejecutara el while.
+while bandera == True:
+    # Solicitamos el ingreso de un numero y convertirmos el dato en entero
+    numeroMes = int(input('Ingrese el numero de un mes: '))
+    print('')
+    # Validamos que el numero se encuentre dentro del rango del 1 a 12
+    if numeroMes > 12:
+        # En caso se mayor que 12, saldra un Error
+        print('Error.....!!!')
+    else:
+        # Regla de Negocio: Si ingresas el valor cero, se termina el programa
+        if numeroMes == 0:
+            bandera = False
+        else:
+            # Al numero ingesado por el usuario le restamos uno para que coincida
+            # con el indice verdadero del mes.
+            index = numeroMes - 1
+            print(meses[index])
